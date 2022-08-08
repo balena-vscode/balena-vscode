06a042802546206f1d3172b115f65caf109cadee
Kaleb Pace <kaleb.pace@pm.me>
2022-08-08 17:02:59 -0500

    feat: initial balena authentication

---
+++ README.md
@@ -0,0 +1,12 @@
+# Balena VS Code (pre-alpha)
+
+This is an unofficial VS Code extension for use with Balena SDK and CLI. It aims to provide features such as:
+- Connecting to a fleet and listing contained devices
+- Online/offline statuses and metadata per device
+- Opening device SSH sessions in the integrated terminal
+- Dropping devices into local mode
+- Modifying fleet and device level environment variables
+- Opening and following device logs in a new tab
+- Managing and pushing releases 
+
+It is an extremely early work-in-progress. Highly advised not to use unless doing extension development. 
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,59 @@
 import * as vscode from 'vscode';
 
+import { BalenaSDK, getSdk } from 'balena-sdk';
+
+import { ExtensionContext, window } from 'vscode'
+
+
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello Worlds!');
+	const balena = getSdk()
+	console.log(`Balena SDK Version: ${balena.version}`)
+
+	const disposable = vscode.commands.registerCommand('balena-vscode.loginToBalenaCloud', async () => {
+
+		// TODO: Refactor into Auth Provider?
+		// https://github.com/microsoft/vscode-extension-samples/tree/main/authenticationprovider-sample
+		balena.auth.isLoggedIn().then(async status => {
+			console.log(`Is logged in: ${status}`)
+			if (!status) {
+				await loginWithToken(balena)
+			}
+
+			balena.models.device.getAll().then((devices: any) => {
+				console.log(devices)
+				const online = devices.filter((d: any) => d.is_online)
+				const offline = devices.filter((d: any) => !d.is_online)
+				window.showInformationMessage(`Success! ${devices.length} devices found: ${online.length} online / ${offline.length} offline`)
+			})
+		})
+
 	});
+
 	context.subscriptions.push(disposable);
+}
+
+export async function loginWithToken(client: BalenaSDK) {
+	const result = (await window.showInputBox({
+		value: 'Balena Access Token',
+		placeHolder: 'For example: \"34mrUxTY8yzlNXJsx8eFqOjy7oPmmfTK\"',
+		validateInput: text => {
+			let msg = null
+
+			if (text.trim().length !== 32) {
+				msg = 'Must be 32 characters long!'
+			}
+
+			if (text.trim().includes(" ")) {
+				msg = 'Contains whitespace characters!'
+			}
+
+			return msg
+		}
+	}))?.trim()
+
+	if (result !== undefined && result !== '') {
+		await client.auth.loginWithToken(result)
+	} else {
+		window.showErrorMessage('Invalid token received: could not authenticate')
+	}
 }
