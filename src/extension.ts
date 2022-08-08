fadb9a5a051621559cec92a717fead82bf797a82
Kaleb Pace <kaleb.pace@pm.me>
2022-08-08 10:53:42 -0500

    feat: bump vscode engine version

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Hello Worlds!');
 	});
 	context.subscriptions.push(disposable);
 }
